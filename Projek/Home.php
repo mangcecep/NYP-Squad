8a5a18a8352ca3b654807f5659f93206fcaca44b
Ganzy-repo <afganaryadi@gmail.com>
2025-09-09 09:22:38 +0700

    Delete Projek directory

--- Projek/Home.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$title = "Late Mate";
-include "./template/header.php";
-?>
-<h1 class="text-center">HOME</h1>
--- Projek/template/footer.php
+++
@@ -1,1 +0,0 @@
-<script src="script/bootstrap.bundle.min.js"></script>
